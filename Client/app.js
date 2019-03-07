f1a70d90bdb170ff7cf590b6882d75de81f0cfb1
Christopher Leabeter <christopherleabeter@gmail.com>
2019-03-07 01:13:20 -0500

    Update socket argument to object to include chatter's initials

--- Client/app.js
+++ Client/app.js
@@ -1,8 +1,19 @@
 var socket = io();
 
 $('form').on('submit',function () {
-  var text = $('#message').val();
-  socket.emit('message', text);
+  var msgData = {}
+  msgData.intls = $('#initials').val().toUpperCase();
+  msgData.txt = $('#message').val();
+  // var initials = $('#initials').val();
+  // var text = $('#message').val();
+  socket.emit('message', msgData);
+  // socket.emit('message', text);
   $('#message').val('');
+  $('#initials').val('');
   return false;
+});
+
+socket.on('message', function (msg) {
+  // $('<li>').text('says: ' + msg).appendTo('#chat-history');
+  $('<li>').text(msg.intls + ' says: ' + msg.txt).appendTo('#chat-history');
 });
